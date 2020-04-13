07db949b44634b2e071b8f758847dde1e6f6baa0
Basak ulcay <55965449+basakulcay@users.noreply.github.com>
2020-04-13 10:24:44 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,34 @@
+var userName='Basak';
+var userQuestion='Will I get a job in tech?';
+var randomNumber=Math.floor(Math.random()*8);
+let eightBall='';
+
+userName ? console.log(`Hello ${userName}`):console.log('Hello!');
+console.log(`${userName} has asked: ${userQuestion}`);
+
+switch(randomNumber) 
+{
+  case 0 :eightBall='It is certain';
+  break;
+  case 1 : eightBall='It is decidedly so';
+  break;
+  case 2 : eightBall='Reply hazy try again';
+  break;
+  case 3 : eightBall='Cannot predict now';
+  break;
+  case 4 : eightBall='Do not count on it';
+  break;
+  case 5 : eightBall='My sources say no';
+  break;
+  case 6 : eightBall='Outlook not so good';
+  break;  
+  case 7 : eightBall='Sings point to yes';
+  break; 
+  default : eightBall='It is not valid';
+  break;
+}
+
+console.log(`The eight ball answered: ${eightBall}`); 
+
+
+
